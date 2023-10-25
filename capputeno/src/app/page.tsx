74644f7e582991695ac9a2d9352b8c3feffce323
Morgana Oliveira <morgana.ifce.2019@gmail.com>
2023-10-25 18:43:39 -0300

    feat: initial products fetch

--- capputeno/src/app/page.tsx
+++ capputeno/src/app/page.tsx
@@ -1,8 +1,18 @@
-import styles from './page.module.css'
+"use client"
+
+import { FilterBar } from '@/components/filter-bar';
+import { ProductList } from '@/components/products-list';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import styles from './page.module.css';
 
 export default function Home() {
+  const client = new QueryClient(); 
   return (
-    <main className={styles.main}>
-    </main>
+    <QueryClientProvider client={client}>
+      <main className={styles.main}>
+        <FilterBar />
+        <ProductList/>
+      </main>
+    </QueryClientProvider>
   )
 }
---
+++ capputeno/src/components/products-list.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { useProducts } from "@/hooks/useProducts";
+
+interface ProductListProps {
+
+}
+
+export function ProductList(props: ProductListProps) {
+  const { data } = useProducts();
+  console.log(data);
+  return (
+    <>
+    </>
+  )
+}
